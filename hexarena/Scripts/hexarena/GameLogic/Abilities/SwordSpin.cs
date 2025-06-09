cd71680167852217bb9f739eb9a834b4f32eece6
bubleman1 <tgrobot5@gmail.com>
2025-06-09 21:30:47 +0300

    Gotta SWEEP SWEEP SWEEP
    Gotta SWEEP SWEEP SWEEP
    Gotta SWEEP SWEEP SWEEP
    Gotta SWEEP SWEEP SWEEP
    Gotta SWEEP SWEEP SWEEP
    Gotta SWEEP SWEEP SWEEP
    Gotta SWEEP SWEEP SWEEP
    Gotta SWEEP SWEEP SWEEP

--- hexarena/Scripts/hexarena/GameLogic/Abilities/SwordSpin.cs
+++ hexarena/Scripts/hexarena/GameLogic/Abilities/SwordSpin.cs
@@ -5,7 +5,7 @@ using Interfaces;
 namespace GameLogic{
 	public class SwordSpin : Active
 	{
-		private int damage = 3;
+		private int damage = 2;
 		public SwordSpin(EventManager eventManager, SurroundSelfTarget targeting)
 		{
 			Connect(eventManager);
