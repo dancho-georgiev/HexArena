a01b1e1843d2cc1edba975ae7a07eddb0105345a
bubleman1 <tgrobot5@gmail.com>
2025-06-11 16:18:46 +0300

    basicattack unfinished

---
+++ hexarena/Scripts/hexarena/GameLogic/Abilities/BasicAttack.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+using Interfaces;
+
+//DOES WORk
+namespace GameLogic{
+	
+	//BasicAttack
+	public partial class BasicAttack : Active
+	{
+		public int Damage {get; protected set;}
+		
+		public BasicAttack(EventManager _eventManager, SingleTarget _targeting){ 
+			Damage = 2;
+			Connect(_eventManager);
+			AddTarget(_targeting);
+		}
+		
+		public override void Connect(EventManager eventManager){
+			eventManager.ActivateAbility1 += Use;
+		}
+		public override void Use()
+		{
+			Targets[0].TargetList[0].TakeDamage(Damage);
+		}
+	}
+}
