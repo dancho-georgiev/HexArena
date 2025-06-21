198df04b6676212030f1880d073d8c8ec1676b6f
bubleman1 <tgrobot5@gmail.com>
2025-06-21 16:42:51 +0300

    smol push
    useless

--- hexarena/AbilitiesDisplay.cs
+++ hexarena/AbilitiesDisplay.cs
@@ -4,7 +4,8 @@ using GameLogic;
 using Interfaces;
 using System.Collections.Generic;
 
-namespace View{
+namespace View
+{
 	public partial class AbilitiesDisplay : HBoxContainer
 	{ 
 		
@@ -20,6 +21,9 @@ namespace View{
 		// and only show it when needed. (Also create one for every possible summon). (pro)
 		
 		public EventManager eventManager;
+		private Node ButtonsContainer;
+		private List<AbilityButton> DisplayedButtons = new List<AbilityButton>();
+		
 		public override void _Ready(){
 			
 		}
@@ -30,12 +34,19 @@ namespace View{
 			eventManager.OnCharacterSelected -= onCharacterSelect;
 		}
 		private void onCharacterSelect(IPlayer character){
-			foreach(IActive activeAbility in character.ActiveAbilities){
-				Button abilityButton = new AbilityButton(activeAbility);
-				abilityButton.CustomMinimumSize = new Vector2(20, 20);
-				abilityButton.ExpandIcon = true;
-				abilityButton.Icon = GD.Load<Texture2D>("res://Assets/AbilityIcons/PitchforkPoke.png");
-				abilityButton.Pressed += AbilityClicked;
+			
+			ButtonsContainer.Free();
+			ButtonsContainer = new Node();
+			
+				foreach(IActive activeAbility in character.ActiveAbilities){
+					Button abilityButton = new AbilityButton(activeAbility);
+					abilityButton.CustomMinimumSize = new Vector2(100, 100);
+					abilityButton.ExpandIcon = true;
+					abilityButton.Icon = GD.Load<Texture2D>("res://Assets/AbilityIcons/PitchforkPoke.png");
+					abilityButton.Pressed += AbilityClicked;
+					ButtonsContainer.AddChild(abilityButton);
+				}
+			this.AddChild(ButtonsContainer);
 				
 				//In the future, also display the Name, EffectDescription and FlavorDescription of the Ability.
 				//Variant 1 add an on-hover event. The event should display a floating textbox with the description
@@ -43,7 +54,6 @@ namespace View{
 				//Variant 2 display the description of the ability in a pre-defined empty text box,
 				//which is attached on one of the sides of the AbilitiesDisplay
 				
-			}
 		}
 		private void AbilityClicked()
 		{
